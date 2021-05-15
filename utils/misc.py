c6a1fd5e15846c541fb58e9d0cc13f269c6f2980
Che <43485962+c-git@users.noreply.github.com>
2021-05-15 01:00:05 -0400

    Use ID for players and Nickname instead of name for display

--- utils/misc.py
+++ utils/misc.py
@@ -1,7 +1,7 @@
 def get_user_info(user):
     # TODO Take nickname if they have one instead of the name
-    ref_name = str(user)
-    display_name = user.name
+    ref_name = user.id
+    display_name = user.display_name
     return ref_name, display_name
 
 
