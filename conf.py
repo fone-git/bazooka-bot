24f169e846ec7d64b8653ca4f9d3227df3b97c08
Che <43485962+c-git@users.noreply.github.com>
2021-05-27 08:41:46 -0400

    Version Bump

--- conf.py
+++ conf.py
@@ -20,7 +20,7 @@ class DBKeys:  # Database key values
 
 class Conf:
     BOT_DESCRIPTION = "Bazooka Alliance BOT"
-    VERSION = '1.10.2'
+    VERSION = '1.10.3'
     LOG_LEVEL = logging.INFO
     COMMAND_PREFIX = 'bb'
     SAVE_CACHE_DELAY = 30  # Minimum number of seconds between saves
