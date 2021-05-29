5b7badbda620a9387475af65f41b45b8aedbe8dd
Che <43485962+c-git@users.noreply.github.com>
2021-05-29 14:02:58 -0400

    Make player ID immutable
    - To provide support for hashing and equality checks

--- bot/common/player.py
+++ bot/common/player.py
@@ -4,12 +4,33 @@ import discord
 
 
 class Player:
+    """
+    Represents a discord User as a Player.
+    Uses the users discord ID. And this field is immutable to support
+    hashing and equality checks.
+
+    WARNING: Two players with different display names but the same ID are
+    considered equal and ALL players with ID "None" are considered equal
+    """
+
     def __init__(self, id_: Union[int, None], display: str, disp_id: Any, *,
                  is_dummy: bool = False):
-        self.id = id_
+        self._id = id_
         self.display = display
         self.disp_id = disp_id
         self.is_dummy = is_dummy  # Used to mark place holders
+
+    @property
+    def id(self):
+        return self._id
+
+    def __eq__(self, other):
+        if isinstance(other, Player):
+            return self.id == other.id
+        return NotImplemented
+
+    def __hash__(self):
+        return hash(self.id)
 
     @property
     def mention(self):
