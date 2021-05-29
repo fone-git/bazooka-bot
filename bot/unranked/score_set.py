37d40635b70eb3b48f30b5e574279b3375c72f0c
Che <43485962+c-git@users.noreply.github.com>
2021-05-29 14:52:38 -0400

    Implement Unranked Functionality

--- bot/unranked/score_set.py
+++ bot/unranked/score_set.py
@@ -1,5 +1,5 @@
-from dataclasses import dataclass
-from typing import Set
+from dataclasses import dataclass, field
+from typing import Set, Union
 
 from bot.common.player import Player
 
@@ -7,16 +7,42 @@ from bot.common.player import Player
 @dataclass
 class ScoreSet:
     score: int
-    players: Set[Player]
+    players: Set[Player] = field(default_factory=set)
+    _str_disp: Union[str, None] = None
 
-    def add_player(self, player: Player):
+    def add(self, player: Player):
+        """
+        Adds the player if there were not already there otherwise does nothing
+        :param player: The player to be added
+        """
         self.players.add(player)
+        self.invalidate_calculated()
 
-    def remove_player(self, player: Player):
+    def remove(self, player: Player):
+        """
+        Removes the player passed (or does nothing)
+        :param player: Player to be removed
+        """
         self.players.remove(player)
+        self.invalidate_calculated()
 
     def has_players(self):
         return len(self.players) > 0
 
+    def invalidate_calculated(self):
+        self._str_disp = None
+
     def __str__(self):
-        return f'{self.score} - {self.players}'
+        if self._str_disp is None:
+            self._str_disp = f'{self.score} - {self.players_as_str()}'
+        return self._str_disp
+
+    def players_as_str(self):
+        result = ""
+        for player in self.players:
+            result += f'{player}, '
+        if result != "":
+            # Crop of trailing ", "
+            result = result[:-2]
+
+        return result
--- bot/unranked/unranked.py
+++ bot/unranked/unranked.py
@@ -1,19 +1,65 @@
+from discord.ext import commands
+
 from bot.common.player import Player
+from bot.unranked.score_set import ScoreSet
+from conf import Conf
 
 
 class Unranked:
+    """
+    Maintains players grouped by score (order with score is Nondeterministic).
+
+    INVARIANTS:
+        - player_lookup and score_lookup are always in sync.
+          either holds all the information by provides quick alternative means
+          of quick access depending on the data you start with.
+    """
+
     def __init__(self):
-        self.player_lookup = {}
-        self.score_lookup = {}
+        self.message = ""
+
+        self.player_lookup = {}  # Maps from player to a score_set object
+        self.score_lookup = {}  # Maps from score to score_set object
+
+        for i in range(Conf.Unranked.MAX_SCORE + 1):
+            self.score_lookup[i] = ScoreSet(i)
 
     def score(self, player: Player, score: int):
-        # TODO Implement
-        pass
+        # Validate score
+        if not (0 <= score <= Conf.Unranked.MAX_SCORE):
+            raise commands.errors.UserInputError(
+                f'Score must be in 0 to {Conf.Unranked.MAX_SCORE} but got '
+                f'{score}')
+
+        # Check if the player is already in data
+        score_set: ScoreSet = self.player_lookup.get(player)
+
+        if score_set is not None:
+            if score_set.score == score:
+                # If same score then exit
+                return
+            else:
+                score_set.remove(player)
+
+        # If code gets to here then the player has been removed (because of
+        # different score) or did not already exist.
+        #
+        # So now simply add the player
+        self.score_lookup[score].add(player)
 
     def remove_player(self, player):
-        # TODO Implement
-        pass
+        score_set: ScoreSet = self.player_lookup.get(player)
+        if score_set is None:
+            raise commands.errors.UserInputError(f'{player} not found!')
+        score_set.remove(player)
 
     def set_msg(self, msg):
-        # TODO Implement
-        pass
+        self.message = msg
+
+    def __str__(self):
+        result = f'UNRANKED CHALLENGE\n{self.message}\nRankings:'
+        for i in range(Conf.Unranked.MAX_SCORE + 1):
+            game_set = self.score_lookup[i]
+            if game_set.has_players():
+                result += f'{game_set}'
+        return result
