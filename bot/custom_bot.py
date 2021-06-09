0bf8e1711da364d17893e407e201fac2ad6031b5
Che <43485962+c-git@users.noreply.github.com>
2021-06-09 01:05:24 -0400

    Move welcome message to config

--- bot/custom_bot.py
+++ bot/custom_bot.py
@@ -125,16 +125,8 @@ class Bot(commands.Bot):
 
         @self.event
         async def on_member_join(member):
-            await member.guild.system_channel.send(
-                f"Welcome {member.mention}. Nice to have you here. Make "
-                f"yourself at home. If you are looking to organize a race "
-                f"tie check out our <#744875911119110245>. If you are "
-                f"looking to "
-                f"join "
-                f"one of our alliances please post a screen shot of your "
-                f"previous season achievements in <#760521977072713758>. "
-                f"Otherwise you're welcome to just chill or Gents are also "
-                f"welcome to check out our <#757542001708105739>.")
+            await member.guild.system_channel.send(conf.WELCOME_MSG.substitute(
+                mention=member.mention))
 
     def get_tournament_as_html(self):
         return self.cog_tournament.as_html()
