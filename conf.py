102fd72f28444c6976875b4d6de2631919444bbb
Che <43485962+c-git@users.noreply.github.com>
2022-10-23 14:52:30 -0400

    Version Bump

--- conf.py
+++ conf.py
@@ -35,7 +35,7 @@ class DBKeys:  # Database key values
 
 class Conf:
     BOT_DESCRIPTION = "Bazooka Alliance BOT"
-    VERSION = '1.29'
+    VERSION = '1.30'
     LOG_LEVEL = logging.INFO
     DB_CACHE_PURGE_LOGLEVEL = logging.DEBUG
     COMMAND_PREFIX = 'bb'
